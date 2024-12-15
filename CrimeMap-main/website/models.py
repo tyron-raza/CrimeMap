9e99c94c76c3ab7c76d5e9ff5444a200ec5985a9
kd-29 <mahiur.rahman.labib@gmail.com>
2024-12-15 12:44:37 +0600

    Add files via upload

---
+++ CrimeMap-main/website/models.py
@@ -0,0 +1,32 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Crime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    location = db.Column(db.String(100))
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    crimes = db.relationship('Crime')
+
+class Category(db.Model):
+    cat_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    count = db.Column(db.Integer, default=0)
+    
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    crime_reported = db.Column(db.String(10000))
+    count = db.Column(db.Integer, default=0)
+    emergency_number = db.Column(db.String(15))
